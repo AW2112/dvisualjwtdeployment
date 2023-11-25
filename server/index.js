c8b3a083c72c9996d82c90dd6e8e3372d8ddc3dc
AW2112 <125974742+AW2112@users.noreply.github.com>
2023-11-25 14:39:18 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,219 +1,235 @@
 const express = require('express');
 const mysql = require('mysql');
-const cors = require('cors');
+const cors = require("cors");
 const bcrypt = require('bcrypt');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const { v4: uuidv4 } = require('uuid');
-
+const bodyParser = require("body-parser");
+const cookieparser = require("cookie-parser");
+const session = require("express-session");
 const saltRounds = 10;
+const { v4: uuidv4 } = require('uuid');
 
 const PORT = process.env.PORT || 8000;
 const BaseURL = process.env.URL || '';
 const app = express();
 
-// Use the default in-memory session store
-app.use(
-  session({
-    secret: 'abdullah',
-    resave: true,
-    saveUninitialized: true,
-    cookie: {
-      secure: false, // Set to true if your site is served over HTTPS
-      maxAge: 60000, // Set the session duration in milliseconds
-    },
-  })
-);
-
-app.use(
-  cors({
+app.use(cors({
     origin: 'https://dvisual-five.vercel.app',
     credentials: true,
-  })
-);
+}));
 app.use(express.json());
-app.use(cookieParser());
+app.use(cookieparser());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    key: "userId",
+    secret: "abdullah",
+    resave: true,
+    saveUninitialized: true,
+}))
 
-// Your existing MySQL connection setup
 const db = mysql.createConnection({
-  host: 'database-1.caoacq3ev5m0.eu-north-1.rds.amazonaws.com',
-  user: 'abdullah',
-  password: 'abdullah-w-21',
-  database: 'dvisual',
+    host: 'database-1.caoacq3ev5m0.eu-north-1.rds.amazonaws.com',
+    user: 'abdullah',
+    password: 'abdullah-w-21',
+    database: 'dvisual'
+})
+
+app.get("/", (req, res) => {
+    res.send("hi");
+})
+
+app.post("/register", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const organisationname = req.body.organisationname;
+
+    // Generate unique ids using UUID
+    const organisation_id = uuidv4();
+    const role_id = uuidv4();
+
+    // Set a default value of 'True' for status
+    const status = true;
+
+    // Generate a random activation date between now and 30 days in the future
+    const currentDate = new Date();
+    const futureDate = new Date();
+    futureDate.setDate(currentDate.getDate() + 30);
+
+    // Generate a random date within the specified range
+    const randomDate = new Date(
+        currentDate.getTime() + Math.random() * (futureDate.getTime() - currentDate.getTime())
+    );
+
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+        if (err) {
+            console.log(err);
+            return res.send({ msg: "Error while hashing password" });
+        }
+
+        const data = {
+            email: email,
+            password: hash,
+            organisationname: organisationname,
+            organisation_id: organisation_id,
+            status: status,
+            activedate: randomDate,
+        };
+
+        let sqll = `SELECT * FROM users WHERE email='${email}'`;
+        db.query(sqll, (er, ress) => {
+            if (ress.length > 0) {
+                res.send({ msg: "User Email Already Present" });
+            } else {
+                // Insert into the organizations table
+                let organizationSql = "INSERT INTO organizations (organisation_id, organisationname) VALUES (?, ?)";
+                let organizationValues = [organisation_id, organisationname];
+
+                db.query(organizationSql, organizationValues, (orgErr, orgResult) => {
+                    if (orgErr) {
+                        console.log(orgErr);
+                        return res.send({ msg: "Error while adding organization" });
+                    }
+
+                    // Now that the organisation_id and organisationname are inserted, proceed to insert into the users table
+                    let sql = "INSERT INTO users SET ?";
+                    db.query(sql, data, (err, result) => {
+                        if (err) {
+                            console.log(err);
+                            return res.send({ msg: "Error while registering user" });
+                        }
+
+                        // Now that the user is registered, you can get the user_id
+                        const user_id = result.insertId;
+
+                        // Set the user_id and organisation_id for the roleData
+                        const roleData = {
+                            role_id: role_id,
+                            user_id: user_id,
+                            role: "user",
+                        };
+
+                        db.query("INSERT INTO user_roles SET ?", roleData, (roleErr, roleResult) => {
+                            if (roleErr) {
+                                console.log(roleErr);
+                                return res.send({ msg: "Error while assigning role" });
+                            }
+
+                            return res.send({ msg: "User registered successfully" });
+                        });
+                    });
+                });
+            }
+        });
+    });
 });
 
-app.get('/', (req, res) => {
-  res.send('hi');
+
+
+
+app.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    let sql = `SELECT * FROM users WHERE email='${email}'`;
+    db.query(sql, (err, result) => {
+        if (err) {
+            console.log(err);
+            return res.send({ msg: "Error while retrieving user information" });
+        }
+        if (result.length > 0) {
+            bcrypt.compare(password, result[0].password, (err, response) => {
+                if (response) {
+                    req.session.user = result;
+                    return res.send({ login: true, useremail: email });
+                } else {
+                    return res.send({ login: false, msg: "Wrong Password" });
+                }
+            });
+        } else {
+            return res.send({ login: false, msg: "User Email Not Exists" });
+        }
+    });
 });
 
-app.post('/register', async (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  const organisationname = req.body.organisationname;
-
-  const organisation_id = uuidv4();
-  const role_id = uuidv4();
-  const status = true;
-  const currentDate = new Date();
-  const futureDate = new Date();
-  futureDate.setDate(currentDate.getDate() + 30);
-  const randomDate = new Date(
-    currentDate.getTime() + Math.random() * (futureDate.getTime() - currentDate.getTime())
-  );
-
-  try {
-    const hash = await bcrypt.hash(password, saltRounds);
-
-    const data = {
-      email: email,
-      password: hash,
-      organisationname: organisationname,
-      organisation_id: organisation_id,
-      status: status,
-      activedate: randomDate,
-    };
-
-    const userExists = await db.query('SELECT * FROM users WHERE email=?', [email]);
-
-    if (userExists.length > 0) {
-      return res.send({ msg: 'User Email Already Present' });
-    }
-
-    await db.beginTransaction();
-
-    const organizationSql = 'INSERT INTO organizations (organisation_id, organisationname) VALUES (?, ?)';
-    const organizationValues = [organisation_id, organisationname];
-
-    await db.query(organizationSql, organizationValues);
-
-    const result = await db.query('INSERT INTO users SET ?', data);
-
-    const user_id = result.insertId;
-
-    const roleData = {
-      role_id: role_id,
-      user_id: user_id,
-      role: 'user',
-    };
-
-    await db.query('INSERT INTO user_roles SET ?', roleData);
-
-    await db.commit();
-
-    return res.send({ msg: 'User registered successfully' });
-  } catch (error) {
-    await db.rollback();
-    console.error('Error during registration:', error);
-    return res.status(500).send({ msg: 'Error while registering user' });
-  }
-});
-
-app.post('/login', async (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-
-  if (req.session.user) {
-    return res.send({ login: true, useremail: email });
-  }
-
-  try {
-    const result = await db.query('SELECT * FROM users WHERE email=?', [email]);
-
-    if (result.length > 0) {
-      const passwordMatch = await bcrypt.compare(password, result[0].password);
-
-      if (passwordMatch) {
-        req.session.user = result[0];
-        return res.send({ login: true, useremail: email });
-      } else {
-        return res.send({ login: false, msg: 'Wrong Password' });
-      }
-    } else {
-      return res.send({ login: false, msg: 'User Email Not Exists' });
-    }
-  } catch (error) {
-    console.error('Error during login:', error);
-    return res.status(500).send({ msg: 'Error while retrieving user information' });
-  }
-});
 
 app.get('/login', (req, res) => {
-  try {
-    if (req.session && req.session.user) {
-      return res.send({ login: true, user: req.session.user });
+    if (req.session.user) {
+        return res.send({ login: true, user: req.session.user });
     } else {
-      return res.send({ login: false });
+        return res.send({ login: false });
     }
-  } catch (error) {
-    console.error('Error in /login:', error);
-    return res.status(500).send({ login: false, error: 'Internal Server Error' });
-  }
 });
 
 app.get('/logout', (req, res) => {
-  req.session.destroy((err) => {
-    if (err) {
-      console.error('Logout error:', err);
-      return res.status(500).json({ success: false, message: 'Logout failed' });
-    }
-
-    res.clearCookie('connect.sid');
-
-    res.status(200).json({ success: true, message: 'Logout successful' });
-  });
-});
-
-app.get('/organization/:userId', (req, res) => {
-  const userId = req.params.userId;
-
-  const sql =
-    'SELECT o.organisationname FROM users u JOIN organizations o ON u.organisation_id = o.organisation_id WHERE u.id = ?';
-
-  db.query(sql, [userId], (err, result) => {
-    if (err) {
-      console.error('Error fetching organization data:', err);
-      res.status(500).json({ error: 'Internal Server Error' });
-    } else {
-      if (result.length > 0) {
-        const organizationname = result[0].organisationname;
-        res.json({ organizationname });
-      } else {
-        res.status(404).json({ error: 'Organization not found' });
+    req.session.destroy((err) => {
+      if (err) {
+        console.error('Logout error:', err);
+        return res.status(500).json({ success: false, message: 'Logout failed' });
       }
-    }
+  
+      res.clearCookie('connect.sid');
+  
+      res.status(200).json({ success: true, message: 'Logout successful' });
+    });
   });
-});
 
-app.post('/add-site', async (req, res) => {
-  const siteName = req.body.site_name;
-  const siteLocation = req.body.site_location;
+  app.get('/organization/:userId', (req, res) => {
+    const userId = req.params.userId;
+  
+    // Query the database to get the organization name based on userId
+    const sql = 'SELECT o.organisationname FROM users u JOIN organizations o ON u.organisation_id = o.organisation_id WHERE u.id = ?';
+  
+    db.query(sql, [userId], (err, result) => {
+      if (err) {
+        console.error('Error fetching organization data:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+      } else {
+        if (result.length > 0) {
+          const organizationname = result[0].organisationname;
+          res.json({ organizationname });
+        } else {
+          res.status(404).json({ error: 'Organization not found' });
+        }
+      }
+    });
+  });
 
-  try {
-    const userId = req.session.user.id;
-    const organizationId = req.session.user.organisation_id;
-    const siteId = uuidv4();
 
-    await db.query('INSERT INTO sites (site_id, organisation_id, site_name, site_location) VALUES (?, ?, ?, ?)', [
-      siteId,
-      organizationId,
-      siteName,
-      siteLocation,
-    ]);
-
-    res.status(200).json({ success: true, siteId });
-  } catch (error) {
-    console.error('Error adding site:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
+  app.post('/add-site', (req, res) => {
+    const siteName = req.body.site_name;
+    const siteLocation = req.body.site_location;
+    //console.log(req.session.user[0]);
+  //console.log(req.body);
+    
+    // Fetch the logged-in user's data
+    const userId = req.session.user[0].id; // Assuming you store user ID in the session
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+  
+    const organizationId =   req.session.user[0].organisation_id;
+  
+      
+      const siteId = uuidv4();
+  
+      // Insert the new site into the sites table
+      const insertSiteSql = "INSERT INTO sites (site_id, organisation_id, site_name, site_location) VALUES (?, ?, ?, ?)";
+      db.query(insertSiteSql, [siteId, organizationId, siteName, siteLocation], (siteErr, siteResult) => {
+        if (siteErr) {
+          console.error('Error adding site:', siteErr);
+          return res.status(500).json({ error: 'Internal Server Error' });
+        }
+  
+        return res.status(200).json({ success: true, siteId });
+      });
+    });
+  
+  
+ 
 app.get('/sites/:organizationId', (req, res) => {
   const organizationId = req.params.organizationId;
 
+  // Replace this with your database query to fetch sites based on organization ID
   const getSitesSql = 'SELECT * FROM sites WHERE organisation_id = ?';
-
   db.query(getSitesSql, [organizationId], (err, result) => {
     if (err) {
       console.error('Error fetching sites:', err);
@@ -226,9 +242,9 @@ app.get('/sites/:organizationId', (req, res) => {
 
 app.get('/sensors/:siteId', (req, res) => {
   const siteId = req.params.siteId;
+  //console.log('Requested siteId:', siteId);
 
   const getSensorNamesSql = 'SELECT sensorname FROM master WHERE site_id = ?';
-
   db.query(getSensorNamesSql, [siteId], (err, result) => {
     if (err) {
       console.error('Error fetching sensor names:', err);
@@ -236,16 +252,19 @@ app.get('/sensors/:siteId', (req, res) => {
     }
 
     const sensorNames = result.map((row) => row.sensorname);
+    //console.log('Retrieved sensor names:', sensorNames);
+
     return res.status(200).json({ sensorNames });
   });
 });
+
 
 app.get('/sensor-data/:sensorName/:siteId', (req, res) => {
   const sensorName = req.params.sensorName;
   const siteId = req.params.siteId;
 
+  // Fetch sensor_id from the master table based on sensorname and site_id
   const getSensorIdSql = 'SELECT sensorid FROM master WHERE sensorname = ? AND site_id = ?';
-
   db.query(getSensorIdSql, [sensorName, siteId], (err, result) => {
     if (err) {
       console.error('Error fetching sensor_id:', err);
@@ -258,14 +277,15 @@ app.get('/sensor-data/:sensorName/:siteId', (req, res) => {
 
     const sensorId = result[0].sensorid;
 
+    // Fetch data from the transactions table based on the obtained sensor_id
     const getSensorDataSql = 'SELECT date, time, reading FROM transactions WHERE sensor_id = ?';
-
     db.query(getSensorDataSql, [sensorId], (err, result) => {
       if (err) {
         console.error('Error fetching sensor data:', err);
         return res.status(500).json({ error: 'Internal Server Error' });
       }
 
+      // Transform the data if needed
       const sensorData = result.map((row) => ({
         date: row.date,
         time: row.time,
@@ -277,6 +297,8 @@ app.get('/sensor-data/:sensorName/:siteId', (req, res) => {
   });
 });
 
+
+
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}`);
+    console.log(`App running on port ${PORT}`);
 });
