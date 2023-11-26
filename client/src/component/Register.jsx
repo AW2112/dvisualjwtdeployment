1eb9eb9f9abd262a26059e84efb49598ce2f444a
AW2112 <125974742+AW2112@users.noreply.github.com>
2023-11-25 16:10:31 -0800

    Update Register.jsx

--- client/src/component/Register.jsx
+++ client/src/component/Register.jsx
@@ -1,13 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
-
 
 const Register = () => {
   const [user, setUser] = useState({
     email: '',
     password: '',
-    organisationname: '', // Only include the organization name
+    organisationname: '',
   });
 
   const [msg, setMsg] = useState('');
@@ -16,111 +15,97 @@ const Register = () => {
 
   const onSub = async (e) => {
     e.preventDefault();
-    // Include only the organization name in the user object
     const userData = {
       email: user.email,
       password: user.password,
       organisationname: user.organisationname,
     };
 
-    let response = await axios.post('https://dvisual-deployment-server.vercel.app/register', userData);
+    try {
+      const response = await axios.post('https://dvisual-deployment-server.vercel.app/register', userData);
 
-    if (response.data.msg) {
-      setMsg(response.data.msg);
-    } else {
-      history.push('/login');
+      if (response.data.msg) {
+        setMsg(response.data.msg);
+      } else {
+        history.push('/login');
+      }
+    } catch (error) {
+      console.error('Error during registration:', error);
     }
   };
 
-  useEffect(() => {
-    const checkLogin = async () => {
-      let response = await axios.get('https://dvisual-deployment-server.vercel.app/login');
-
-      if (response.data.user) {
-        history.push('/profile');
-      }
-    };
-    checkLogin();
-  }, []);
-
   const userInput = (event) => {
     const { name, value } = event.target;
-    setUser((prev) => {
-      return {
-        ...prev,
-        [name]: value,
-      };
-    });
+    setUser((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
   };
 
   return (
-    <>
-      <div
-        className="container"
-        id="formm"
-        style={{
-          backgroundColor: 'black', // Dark background color
-          color: 'white', // Light text color
-        }}
-      >
-        <div className="row">
-          <div className="col-lg-6 col-md-8 col-12 mx-auto">
-            {msg ? (
-              <>
-                <div className="alert alert-danger alert-dismissible">
-                  <button type="button" className="close" data-dismiss="alert">
-                    &times;
-                  </button>
-                  <strong>ERROR!</strong> {msg}
-                </div>
-              </>
-            ) : null}
-            <br />
-            <form onSubmit={onSub}>
-              <div className="form-group">
-                <label>Email address:</label>
-                <input
-                  type="email"
-                  className="form-control"
-                  placeholder="Enter email"
-                  name="email"
-                  value={user.email}
-                  onChange={userInput}
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <label>Password:</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  placeholder="Enter password"
-                  name="password"
-                  value={user.password}
-                  onChange={userInput}
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <label>Organisation Name:</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Enter organization name"
-                  name="organisationname"
-                  value={user.organisationname}
-                  onChange={userInput}
-                  required
-                />
-              </div>
-              <button type="submit" className="btn btn-primary">
-                Submit
+    <div
+      className="container"
+      id="formm"
+      style={{
+        backgroundColor: 'black', // Dark background color
+        color: 'white', // Light text color
+      }}
+    >
+      <div className="row">
+        <div className="col-lg-6 col-md-8 col-12 mx-auto">
+          {msg && (
+            <div className="alert alert-danger alert-dismissible">
+              <button type="button" className="close" data-dismiss="alert">
+                &times;
               </button>
-            </form>
-          </div>
+              <strong>ERROR!</strong> {msg}
+            </div>
+          )}
+          <br />
+          <form onSubmit={onSub}>
+            <div className="form-group">
+              <label>Email address:</label>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Enter email"
+                name="email"
+                value={user.email}
+                onChange={userInput}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label>Password:</label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Enter password"
+                name="password"
+                value={user.password}
+                onChange={userInput}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label>Organisation Name:</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter organization name"
+                name="organisationname"
+                value={user.organisationname}
+                onChange={userInput}
+                required
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
